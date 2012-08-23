09873773e0b59fe73f7d000add96d16b663d1bfd
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2012-08-23 13:10:32 +0200

    core: switch back to grizzly 2.1 as it shows better performance

--- oncrpc4j-core/src/main/java/org/dcache/xdr/GrizzlyXdrTransport.java
+++ oncrpc4j-core/src/main/java/org/dcache/xdr/GrizzlyXdrTransport.java
@@ -26,9 +26,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.glassfish.grizzly.Buffer;
 import org.glassfish.grizzly.Connection;
-import org.glassfish.grizzly.asyncqueue.PushBackContext;
-import org.glassfish.grizzly.asyncqueue.PushBackHandler;
-import org.glassfish.grizzly.asyncqueue.WritableMessage;
 import org.glassfish.grizzly.filterchain.FilterChainContext;
 
 public class GrizzlyXdrTransport implements XdrTransport {
@@ -37,7 +34,6 @@ public class GrizzlyXdrTransport implements XdrTransport {
     private final Connection<InetSocketAddress> _connection;
     private final ReplyQueue<Integer, RpcReply> _replyQueue;
 
-    private static final PushBackHandler RERTY_ON_PUSHBACK = new RetryPushBackHandler();
     private final static Logger _log = LoggerFactory.getLogger(GrizzlyXdrTransport.class);
 
     public GrizzlyXdrTransport(FilterChainContext context, ReplyQueue<Integer, RpcReply> replyQueue) {
@@ -56,7 +52,7 @@ public class GrizzlyXdrTransport implements XdrTransport {
         buffer.allowBufferDispose(true);
 
         // pass destination address to handle UDP connections as well
-        _context.write(_context.getAddress(), buffer, null, RERTY_ON_PUSHBACK);
+        _context.write(_context.getAddress(), buffer, null);
     }
 
     @Override
@@ -82,24 +78,5 @@ public class GrizzlyXdrTransport implements XdrTransport {
     @Override
     public String toString() {
         return getRemoteSocketAddress() + " <=> " + getLocalSocketAddress();
-    }
-
-    /**
-     * An implementation of {@link PushBackHandler}, which will enforce Grizzly's
-     * {@link org.glassfish.grizzly.Writer} to retry when possible if
-     * message can not be neither written nor added to write queue due
-     * to I/O or memory limitations.
-     */
-    private static class RetryPushBackHandler implements PushBackHandler {
-
-        @Override
-        public void onAccept(Connection connection, WritableMessage message) {
-            // NOP
-        }
-
-        @Override
-        public void onPushBack(Connection connection, WritableMessage message, PushBackContext pushBackContext) {
-            pushBackContext.retryWhenPossible();
-        }
     }
 }
