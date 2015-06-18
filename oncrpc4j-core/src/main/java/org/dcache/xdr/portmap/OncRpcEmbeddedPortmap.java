dCache/oncrpc4j

commit f522af90fb61106560a659945daeb93dbca0bc77

rpc: use Future#get instead of CountdownLaunch for sync calls
simplify logic. Blocking calls can throw TimeoutException.

Acked-by: Paul Millar
Target: master

oncrpc4j-core/src/main/java/org/dcache/xdr/portmap/OncRpcEmbeddedPortmap.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009 - 2014 Deutsches Elektronen-Synchroton,
+ * Copyright (c) 2009 - 2015 Deutsches Elektronen-Synchroton,
  * Member of the Helmholtz Association, (DESY), HAMBURG, GERMANY
  *
  * This library is free software; you can redistribute it and/or modify
@@ -21,6 +21,7 @@ package org.dcache.xdr.portmap;
 
 import java.io.IOException;
 import java.net.InetAddress;
+import java.util.concurrent.TimeoutException;
 import org.dcache.xdr.IpProtocolType;
 import org.dcache.xdr.OncRpcClient;
 import org.dcache.xdr.OncRpcException;
@@ -62,7 +63,7 @@ public class OncRpcEmbeddedPortmap {
                         i, _auth, transport);
                 try {
                     call.call(0, XdrVoid.XDR_VOID, XdrVoid.XDR_VOID, timeout);
-                } catch (OncRpcException ex) {}
+                } catch (TimeoutException | OncRpcException e) {}
                 localPortmapperRunning = true;
             }
         } catch (IOException e) {

oncrpc4j-core/src/main/java/org/dcache/xdr/OncRpcSvc.java
@@ -56,6 +56,7 @@ import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 import static org.dcache.xdr.GrizzlyUtils.getSelectorPoolCfg;
 import static org.dcache.xdr.GrizzlyUtils.rpcMessageReceiverFor;
@@ -215,7 +216,7 @@ public class OncRpcSvc {
                     try {
                         portmapClient.setPort(program.getNumber(), program.getVersion(),
                                 netid, uaddr, username);
-                    } catch (OncRpcException e) {
+                    } catch (OncRpcException | TimeoutException e) {
                         _log.warn("Failed to register program: {}", e.getMessage());
                     }
                 }
@@ -248,8 +249,8 @@ public class OncRpcSvc {
                 try {
                     portmapClient.unsetPort(program.getNumber(),
                             program.getVersion(), username);
-                } catch (OncRpcException ex) {
-                    _log.info("Failed to unregister program: {}", ex.getMessage());
+                } catch (OncRpcException | TimeoutException e) {
+                    _log.info("Failed to unregister program: {}", e.getMessage());
                 }
             }
         } catch (RpcProgUnavailable e) {

oncrpc4j-core/src/main/java/org/dcache/xdr/RpcCall.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009 - 2014 Deutsches Elektronen-Synchroton,
+ * Copyright (c) 2009 - 2015 Deutsches Elektronen-Synchroton,
  * Member of the Helmholtz Association, (DESY), HAMBURG, GERMANY
  *
  * This library is free software; you can redistribute it and/or modify
@@ -22,12 +22,13 @@ package org.dcache.xdr;
 import com.google.common.base.Throwables;
 import com.google.common.util.concurrent.SettableFuture;
 import java.io.IOException;
+import java.io.InterruptedIOException;
 import java.nio.channels.CompletionHandler;
-import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicReference;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -371,9 +372,10 @@ public class RpcCall {
      * @param result the result of the procedure
      * @throws OncRpcException
      * @throws IOException
+     * @throws TimeoutException if the wait timed out
      */
     public void call(int procedure, XdrAble args, XdrAble result)
-            throws OncRpcException, IOException {
+            throws OncRpcException, IOException, TimeoutException {
 
         this.call(procedure, args, result, Integer.MAX_VALUE);
     }
@@ -387,47 +389,23 @@ public class RpcCall {
      * @param timeout
      * @throws OncRpcException
      * @throws IOException
+     * @throws TimeoutException if the wait timed out
      */
     public void call(int procedure, XdrAble args, XdrAble result, int timeout)
-            throws OncRpcException, IOException {
+            throws OncRpcException, IOException, TimeoutException {
 
-        final CountDownLatch cdl = new CountDownLatch(1);
-        final XdrAble r = result;
-        final AtomicReference<Throwable> exception = new AtomicReference<>();
-        CompletionHandler<RpcReply, XdrTransport> callback = new CompletionHandler<RpcReply, XdrTransport>() {
-
-            @Override
-            public void completed(RpcReply reply, XdrTransport attachment) {
-                try {
-                    reply.getReplyResult(r);
-                    cdl.countDown();
-                } catch (IOException e) {
-                    failed(e, attachment);
-                }
-            }
-
-            @Override
-            public void failed(Throwable exc, XdrTransport attachment) {
-                exception.set(exc);
-                cdl.countDown();
-            }
-        };
-
-        call(procedure, args, callback);
+        Future<RpcReply> future = call(procedure, args);
 
         try {
-            if (!cdl.await(timeout, TimeUnit.MILLISECONDS)) {
-                _log.info("Did not get reply in time");
-                throw new IOException("Did not get reply in time");
-            }
+            RpcReply reply =  future.get(timeout, TimeUnit.MILLISECONDS);
+            reply.getReplyResult(result);
         } catch (InterruptedException e) {
-            throw new IOException("Did not get reply in time");
-        }
-
-        Throwable t = exception.get();
-        if (t != null) {
+            throw new InterruptedIOException();
+        } catch (ExecutionException e) {
+            Throwable t = Throwables.getRootCause(e);
             Throwables.propagateIfInstanceOf(t, OncRpcException.class);
             Throwables.propagateIfInstanceOf(t, IOException.class);
+
             throw new IOException(t);
         }
     }

oncrpc4j-core/src/main/java/org/dcache/xdr/SimpleRpcClient.java
@@ -20,6 +20,7 @@
 package org.dcache.xdr;
 
 import java.net.InetAddress;
+import java.util.concurrent.Future;
 
 public class SimpleRpcClient {
 
@@ -43,6 +44,9 @@ public class SimpleRpcClient {
          * call PROC_NULL (ping)
          */
         call.call(0, XdrVoid.XDR_VOID, XdrVoid.XDR_VOID);
+
+        Future<RpcReply> r = call.call(0, XdrVoid.XDR_VOID);
+        r.get();
         rpcClient.close();
     }
 }

oncrpc4j-core/src/main/java/org/dcache/xdr/portmap/GenericPortmapClient.java
@@ -21,6 +21,7 @@ package org.dcache.xdr.portmap;
 
 import java.io.IOException;
 import java.net.InetAddress;
+import java.util.concurrent.TimeoutException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.dcache.xdr.IpProtocolType;
@@ -40,11 +41,9 @@ public class GenericPortmapClient implements OncPortmapClient {
 
     public GenericPortmapClient(XdrTransport transport) throws RpcProgUnavailable {
 
-       OncPortmapClient portmapClient = new RpcbindV4Client(new RpcCall(100000, 4,
-               _auth, transport));
+       OncPortmapClient portmapClient = new RpcbindV4Client(new RpcCall(100000, 4, _auth, transport));
         if( !portmapClient.ping() ) {
-            portmapClient = new PortmapV2Client( new RpcCall(100000, 2,
-                    _auth, transport) );
+            portmapClient = new PortmapV2Client( new RpcCall(100000, 2, _auth, transport) );
             if(!portmapClient.ping()) {
                 // FIXME: return correct exception
                 throw new RpcProgUnavailable("portmap service not available");
@@ -54,7 +53,7 @@ public class GenericPortmapClient implements OncPortmapClient {
         _portmapClient = portmapClient;
     }
 
-    public void dump() throws OncRpcException, IOException {
+    public void dump() throws OncRpcException, IOException, TimeoutException {
         _portmapClient.dump();
     }
 
@@ -62,22 +61,22 @@ public class GenericPortmapClient implements OncPortmapClient {
         return _portmapClient.ping();
     }
 
-    public boolean setPort(int program, int version, String netid, String addr, String owner) throws OncRpcException, IOException {
+    public boolean setPort(int program, int version, String netid, String addr, String owner) throws OncRpcException, IOException, TimeoutException {
         return _portmapClient.setPort(program, version, netid, addr, owner);
     }
 
-    public boolean unsetPort(int program, int version, String owner) throws OncRpcException, IOException {
+    public boolean unsetPort(int program, int version, String owner) throws OncRpcException, IOException, TimeoutException {
         return _portmapClient.unsetPort(program, version, owner);
     }
 
-    public String getPort(int program, int version, String netid) throws OncRpcException, IOException {
+    public String getPort(int program, int version, String netid) throws OncRpcException, IOException, TimeoutException {
         return _portmapClient.getPort(program, version, netid);
     }
 
-    public static void main(String[] args) throws InterruptedException, IOException, OncRpcException {
+    public static void main(String[] args) throws InterruptedException, IOException, OncRpcException, TimeoutException {
 
         int protocol = IpProtocolType.TCP;
-        
+
         OncRpcClient rpcClient = new OncRpcClient(InetAddress.getByName(null), IpProtocolType.UDP, 111);
         XdrTransport transport = rpcClient.connect();
 
@@ -97,7 +96,7 @@ public class GenericPortmapClient implements OncPortmapClient {
             System.out.println(portmapClient.setPort(prog, vers, netid, addr, user));
             System.out.println("getport: " + portmapClient.getPort(prog, vers, netid));
             System.out.println("-------");
-            
+
       //      System.out.println(portmapClient.unsetPort(prog, vers, user));
             System.out.println("getport: " + portmapClient.getPort(prog, vers, netid));
 

oncrpc4j-core/src/main/java/org/dcache/xdr/portmap/OncPortmapClient.java
@@ -20,15 +20,16 @@
 package org.dcache.xdr.portmap;
 
 import java.io.IOException;
+import java.util.concurrent.TimeoutException;
 import org.dcache.xdr.OncRpcException;
 
 public interface OncPortmapClient {
 
-    void dump() throws OncRpcException, IOException;
+    void dump() throws OncRpcException, IOException, TimeoutException;
 
     boolean ping();
 
-    boolean setPort(int program, int version, String netid, String addr, String owner) throws OncRpcException, IOException;
-    boolean unsetPort(int program, int version, String owner) throws OncRpcException, IOException;
-    String  getPort(int program, int version, String netid) throws OncRpcException, IOException;
+    boolean setPort(int program, int version, String netid, String addr, String owner) throws OncRpcException, IOException, TimeoutException;
+    boolean unsetPort(int program, int version, String owner) throws OncRpcException, IOException, TimeoutException;
+    String  getPort(int program, int version, String netid) throws OncRpcException, IOException, TimeoutException;
 }

oncrpc4j-core/src/main/java/org/dcache/xdr/portmap/PortmapV2Client.java
@@ -21,6 +21,7 @@ package org.dcache.xdr.portmap;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.util.concurrent.TimeoutException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.dcache.utils.net.InetSocketAddresses;
@@ -40,7 +41,7 @@ public class PortmapV2Client implements OncPortmapClient {
         _call = call;
     }
 
-    public void dump() throws OncRpcException, IOException {
+    public void dump() throws OncRpcException, IOException, TimeoutException {
         _log.debug("portmap dump");
 
         pmaplist list_reply = new pmaplist();
@@ -55,14 +56,13 @@ public class PortmapV2Client implements OncPortmapClient {
         try {
             _call.call(OncRpcPortmap.PMAPPROC_NULL, XdrVoid.XDR_VOID, XdrVoid.XDR_VOID, 2000);
             pong = true;
-        }catch(OncRpcException e) {
-        }catch(IOException e) {
-        }
+        }catch (TimeoutException | IOException e) {}
 
         return pong;
     }
 
-    public boolean setPort(int program, int version, String netids, String addr, String owner) throws OncRpcException, IOException {
+    public boolean setPort(int program, int version, String netids, String addr, String owner)
+            throws OncRpcException, IOException, TimeoutException {
         _log.debug("portmap set port: prog: {} vers: {}, netid: {} addr: {}, owner: {}",
                 new Object[]{program, version, netids, addr, owner});
 
@@ -76,7 +76,8 @@ public class PortmapV2Client implements OncPortmapClient {
     }
 
     @Override
-    public boolean unsetPort(int program, int version, String owner) throws OncRpcException, IOException {
+    public boolean unsetPort(int program, int version, String owner)
+            throws OncRpcException, IOException, TimeoutException {
         _log.debug("portmap unset port: prog: {} vers: {}, owner: {}",
                 new Object[]{program, version, owner});
 
@@ -89,7 +90,8 @@ public class PortmapV2Client implements OncPortmapClient {
     }
 
     @Override
-    public String getPort(int program, int version, String nid) throws OncRpcException, IOException {
+    public String getPort(int program, int version, String nid)
+            throws OncRpcException, IOException, TimeoutException {
 
         mapping m = new mapping(program, version, netid.idOf(nid), 0);
         XdrInt port = new XdrInt();

oncrpc4j-core/src/main/java/org/dcache/xdr/portmap/RpcbindV4Client.java
@@ -20,6 +20,7 @@
 package org.dcache.xdr.portmap;
 
 import java.io.IOException;
+import java.util.concurrent.TimeoutException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.dcache.xdr.OncRpcException;
@@ -49,15 +50,14 @@ public class RpcbindV4Client implements OncPortmapClient {
         try {
             _call.call(OncRpcPortmap.PMAPPROC_NULL, XdrVoid.XDR_VOID, XdrVoid.XDR_VOID, 2000);
             pong = true;
-        }catch(OncRpcException e) {
-        }catch(IOException e) {
+        }catch(IOException | TimeoutException e) {
         }
 
         return pong;
     }
 
     public boolean setPort(int program, int version, String netid, String addr, String owner)
-            throws OncRpcException, IOException {
+            throws OncRpcException, IOException, TimeoutException {
 
         _log.debug("portmap set port: prog: {} vers: {}, netid: {} addr: {}, owner: {}",
                 new Object[] {program, version, netid, addr, owner});
@@ -72,7 +72,7 @@ public class RpcbindV4Client implements OncPortmapClient {
 
     @Override
     public boolean unsetPort(int program, int version, String owner)
-            throws OncRpcException, IOException {
+            throws OncRpcException, IOException, TimeoutException {
 
         _log.debug("portmap unset port: prog: {} vers: {}, owner: {}",
                 new Object[]{program, version, owner});
@@ -86,14 +86,15 @@ public class RpcbindV4Client implements OncPortmapClient {
     }
 
     @Override
-    public String getPort(int program, int version, String netid) throws OncRpcException, IOException {
+    public String getPort(int program, int version, String netid)
+            throws OncRpcException, IOException, TimeoutException {
         rpcb arg = new rpcb(program, version, netid, "", "");
         XdrString xdrString = new XdrString();
         _call.call(OncRpcPortmap.RPCBPROC_GETADDR, arg, xdrString);
         return xdrString.stringValue();
     }
 
-    public void dump() throws OncRpcException, IOException {
+    public void dump() throws OncRpcException, IOException, TimeoutException {
 
         _log.debug("portmap dump");
 

commit bcbd0ef2b9d004b49229de3b27b8753f854de3cd

rpcbind: fix regression introduced in f522af90
by introducing TimeoutException we fail to detect
absence of locally running rpcbind service

Acked-by: Paul Millar
Target: master, 2.4

oncrpc4j-core/src/main/java/org/dcache/xdr/portmap/OncRpcEmbeddedPortmap.java
@@ -60,13 +60,14 @@ public class OncRpcEmbeddedPortmap {
                     IpProtocolType.UDP, OncRpcPortmap.PORTMAP_PORT);
             XdrTransport transport = rpcClient.connect();
             /* check for version 2, 3 and 4 */
-            for (int i = 2; i < 5; i++) {
+            for (int i = 2; i < 5 && !localPortmapperRunning; i++) {
                 RpcCall call = new RpcCall(OncRpcPortmap.PORTMAP_PROGRAMM,
                         i, _auth, transport);
                 try {
                     call.call(0, XdrVoid.XDR_VOID, XdrVoid.XDR_VOID, timeoutValue, timeoutUnit);
+                    localPortmapperRunning = true;
                 } catch (TimeoutException | OncRpcException e) {}
-                localPortmapperRunning = true;
+
             }
         } catch (IOException e) {
         } finally {