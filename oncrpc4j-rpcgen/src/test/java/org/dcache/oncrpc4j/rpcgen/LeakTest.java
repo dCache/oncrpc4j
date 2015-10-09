b5c0f6e272c1155a42563c21184baa88c1f1426e
radai-rosenblatt <radai.rosenblatt@gmail.com>
2015-10-09 11:34:05 -0700

    add test case to demonstrate file/pipe leak on connection refused

---
+++ oncrpc4j-rpcgen/src/test/java/org/dcache/oncrpc4j/rpcgen/LeakTest.java
@@ -0,0 +1,28 @@
+package org.dcache.oncrpc4j.rpcgen;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.net.ConnectException;
+import java.net.InetAddress;
+
+public class LeakTest {
+
+    @Test
+    public void testLeakOnConnectionRefused() throws Throwable {
+        InetAddress localAddress = InetAddress.getByName("localhost");
+        for (int i=0; i<10000; i++) {
+            try {
+                new BlobStoreClient(localAddress, 60666);
+                Assert.fail("connection expected to fail");
+            } catch (Throwable t) {
+                Throwable cause = TestUtil.getRootCause(t);
+                if ((cause instanceof ConnectException) && "Connection refused".equals(cause.getMessage())) {
+                    //this is expected
+                    continue;
+                }
+                throw t;
+            }
+        }
+    }
+}
---
+++ oncrpc4j-rpcgen/src/test/java/org/dcache/oncrpc4j/rpcgen/TestUtil.java
@@ -0,0 +1,12 @@
+package org.dcache.oncrpc4j.rpcgen;
+
+public class TestUtil {
+
+    public static Throwable getRootCause(Throwable t) {
+        Throwable cause = t;
+        while (cause.getCause() != null && cause.getCause() != cause) {
+            cause = cause.getCause();
+        }
+        return cause;
+    }
+}
