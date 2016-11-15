042ab50f117237cd1da5a90a59072c2ae7f8c061
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2016-11-15 16:47:50 +0100

    src: replace doggy code from stachoverflow with alternative from guava
    Result:
    cleaner license
    
    Acked-by: Paul Millar
    Target: master, 2.6

--- oncrpc4j-rpcgen/src/test/java/org/dcache/oncrpc4j/rpcgen/BlobStoreServerImpl.java
+++ oncrpc4j-rpcgen/src/test/java/org/dcache/oncrpc4j/rpcgen/BlobStoreServerImpl.java
@@ -1,5 +1,6 @@
 package org.dcache.oncrpc4j.rpcgen;
 
+import com.google.common.io.BaseEncoding;
 import org.dcache.xdr.RpcCall;
 
 import java.util.concurrent.ConcurrentHashMap;
@@ -47,14 +48,7 @@ public class BlobStoreServerImpl extends BlobStoreServer {
         return requestCounter.get();
     }
 
-    final protected static char[] hexArray = "0123456789ABCDEF".toCharArray();
     public static String bytesToHex(byte[] bytes) {
-        char[] hexChars = new char[bytes.length * 2];
-        for ( int j = 0; j < bytes.length; j++ ) {
-            int v = bytes[j] & 0xFF;
-            hexChars[j * 2] = hexArray[v >>> 4];
-            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
-        }
-        return new String(hexChars);
+        return BaseEncoding.base16().upperCase().encode(bytes);
     }
 }
