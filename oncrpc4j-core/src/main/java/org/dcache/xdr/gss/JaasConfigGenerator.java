2600db31dc2bc786142f23394b7a136b9c898ea1
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2016-11-28 08:23:59 +0100

    rpc: use java.nio.file.Files instead of com.google.common.io.Files
    Motivation:
    do not use external library if there is an alternative from standard
    API.
    
    Modification:
    update usage of com.google.common.io.Files with java.nio.file.Files.
    
    Result:
    less dependency on external library.
    
    Acked-by: Paul Millar
    Target: master

--- oncrpc4j-core/src/main/java/org/dcache/xdr/gss/JaasConfigGenerator.java
+++ oncrpc4j-core/src/main/java/org/dcache/xdr/gss/JaasConfigGenerator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009 - 2015 Deutsches Elektronen-Synchroton,
+ * Copyright (c) 2009 - 2016 Deutsches Elektronen-Synchroton,
  * Member of the Helmholtz Association, (DESY), HAMBURG, GERMANY
  *
  * This library is free software; you can redistribute it and/or modify
@@ -19,11 +19,18 @@
  */
 package org.dcache.xdr.gss;
 
-import com.google.common.io.Files;
-import java.io.BufferedWriter;
-import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.attribute.PosixFilePermissions;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.attribute.FileAttribute;
+import java.nio.file.attribute.PosixFilePermission;
+import java.util.EnumSet;
+import java.util.Set;
+
+import static java.nio.file.attribute.PosixFilePermission.OWNER_READ;
+import static java.nio.file.attribute.PosixFilePermission.OWNER_WRITE;
 
 /**
  * Utility class to generate JAAS configuration file for kerberized service.
@@ -32,7 +39,10 @@ import java.nio.charset.StandardCharsets;
  */
 class JaasConfigGenerator {
 
-    private final static String jaasConfigTemplate = "com.sun.security.jgss.accept {\n"
+    private static final FileAttribute<Set<PosixFilePermission>> OWNER_RW =
+            PosixFilePermissions.asFileAttribute(EnumSet.of(OWNER_READ, OWNER_WRITE));
+
+    private final static String JAAS_CONFIG_TEMPLATE = "com.sun.security.jgss.accept {\n"
             + "  com.sun.security.auth.module.Krb5LoginModule required\n"
             + "  debug=false\n"
             + "  principal=\"%s\"\n"
@@ -46,16 +56,12 @@ class JaasConfigGenerator {
 
     public static String generateJaasConfig(String servicePrincipal, String keytab) throws IOException {
 
-        File jaasFile = File.createTempFile("jaas", ".conf");
-        jaasFile.deleteOnExit();
-        jaasFile.setExecutable(false);
-        jaasFile.setReadable(true, true);
-        jaasFile.setWritable(true, true);
+        Path jaasFile = Files.createTempFile("jaas", ".conf", OWNER_RW);
+        jaasFile.toFile().deleteOnExit();
 
-        String config = String.format(jaasConfigTemplate, servicePrincipal, keytab);
-        try (BufferedWriter bw = Files.newWriter(jaasFile, StandardCharsets.UTF_8)) {
-            bw.write(config, 0, config.length());
-        }
-        return jaasFile.getAbsolutePath();
+        String config = String.format(JAAS_CONFIG_TEMPLATE, servicePrincipal, keytab);
+        Files.write(jaasFile, config.getBytes(StandardCharsets.UTF_8));
+
+        return jaasFile.toAbsolutePath().toString();
     }
 }
