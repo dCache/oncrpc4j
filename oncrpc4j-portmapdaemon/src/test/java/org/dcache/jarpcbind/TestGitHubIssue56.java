0690b08f5651eb56480a558e5527038a52fc2300
Jean-Marie Kubek <jkubek@univ-jfc.fr>
2017-11-02 16:59:38 +0100

    Test case for issue #56 https://github.com/dCache/oncrpc4j/issues/56

---
+++ oncrpc4j-portmapdaemon/src/test/java/org/dcache/jarpcbind/TestGitHubIssue56.java
@@ -0,0 +1,43 @@
+package org.dcache.jarpcbind;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.util.concurrent.TimeoutException;
+
+import org.dcache.xdr.IpProtocolType;
+import org.dcache.xdr.OncRpcClient;
+import org.dcache.xdr.OncRpcProgram;
+import org.dcache.xdr.OncRpcSvc;
+import org.dcache.xdr.OncRpcSvcBuilder;
+import org.dcache.xdr.XdrTransport;
+import org.dcache.xdr.portmap.GenericPortmapClient;
+import org.dcache.xdr.portmap.OncRpcPortmap;
+import org.dcache.xdr.portmap.OncRpcbindServer;
+import org.dcache.xdr.portmap.rpcb;
+import org.junit.Test;
+
+public class TestGitHubIssue56 {
+
+	@Test
+	public void DumpTest() throws IOException, TimeoutException {
+
+		OncRpcSvc rpcbindServer = new OncRpcSvcBuilder()
+                .withTCP()
+                .withUDP()
+                .withoutAutoPublish()
+                .withRpcService(new OncRpcProgram(OncRpcPortmap.PORTMAP_PROGRAMM, OncRpcPortmap.PORTMAP_V2), new OncRpcbindServer())
+                .build();
+		rpcbindServer.start();
+		int protoType = IpProtocolType.TCP;
+		OncRpcClient rpcClient = new OncRpcClient(rpcbindServer.getInetSocketAddress(protoType),protoType );
+		XdrTransport transport = rpcClient.connect();
+		GenericPortmapClient portmapClient = new GenericPortmapClient(transport);
+		for (rpcb r : portmapClient.dump()){
+			assertEquals("administrator",r.getOwner());
+		}
+		
+	}
+
+}
