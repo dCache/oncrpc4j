0802b3bf33946f4b3c6910f091d41052c9fa104a
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2018-09-19 18:22:28 +0200

    portmap: fix xdr encoding of mapping object
    Motivation:
    commit 1783622 have introduced a regression into mapping object by adding
    owner into set of encoded valued. This broke compatibility with standard
    client and servers:
    
    Failed to process RPC request: xdr stream too short
    
    Modification:
    fix broken encoding. Remove rpcb class to mapping class conversion. Update
    code to new changes. Keep semantic raised in issue #56. Use "unknown" as
    owner when dump v2 servers.
    
    Result:
    restored compatibility with other client and servers.
    
    Fixes: #56
    Acked-by: Paul Millar
    Target: master, 3.0

--- oncrpc4j-core/src/main/java/org/dcache/oncrpc4j/portmap/mapping.java
+++ oncrpc4j-core/src/main/java/org/dcache/oncrpc4j/portmap/mapping.java
@@ -32,7 +32,6 @@ public class mapping implements XdrAble {
     private int _vers;
     private int _prot;
     private int _port;
-    private String _owner;
 
     public int getPort() {
         return _port;
@@ -50,23 +49,13 @@ public class mapping implements XdrAble {
         return _vers;
     }
 
-    public String getOwner() {
-        return _owner;
-    }
-
     public mapping() {}
 
-	@Deprecated
     public mapping(int prog, int vers, int prot, int port) {
-    	this(prog,vers,prot,port,"unspecified");
-    }
-
-    public mapping(int prog, int vers, int prot, int port, String owner) {
         _prog = prog;
         _vers = vers;
         _prot = prot;
         _port = port;
-        _owner = owner;
 	}
 
 	@Override
@@ -75,7 +64,6 @@ public class mapping implements XdrAble {
         _vers = xdr.xdrDecodeInt();
         _prot = xdr.xdrDecodeInt();
         _port = xdr.xdrDecodeInt();
-        _owner = xdr.xdrDecodeString();
     }
 
     @Override
@@ -84,12 +72,11 @@ public class mapping implements XdrAble {
         xdr.xdrEncodeInt(_vers);
         xdr.xdrEncodeInt(_prot);
         xdr.xdrEncodeInt(_port);
-        xdr.xdrEncodeString(_owner);
     }
 
     @Override
     public String toString() {
-        return String.format("prog: %d, vers: %d, prot: %s, port: %d, owner %s",
-                _prog, _vers, IpProtocolType.toString(_prot), _port,_owner);
+        return String.format("prog: %d, vers: %d, prot: %s, port: %d",
+                _prog, _vers, IpProtocolType.toString(_prot), _port);
     }
 }
--- oncrpc4j-core/src/test/java/org/dcache/oncrpc4j/rpc/TestGitHubIssue56.java
+++ oncrpc4j-core/src/test/java/org/dcache/oncrpc4j/rpc/TestGitHubIssue56.java
@@ -3,62 +3,52 @@ package org.dcache.oncrpc4j.rpc;
 import static org.junit.Assert.*;
 
 import java.io.IOException;
-import java.net.InetAddress;
 import java.util.concurrent.TimeoutException;
 
 import org.dcache.oncrpc4j.rpc.net.IpProtocolType;
-import org.dcache.oncrpc4j.rpc.OncRpcClient;
-import org.dcache.oncrpc4j.rpc.OncRpcProgram;
-import org.dcache.oncrpc4j.rpc.OncRpcSvc;
-import org.dcache.oncrpc4j.rpc.OncRpcSvcBuilder;
 import org.dcache.oncrpc4j.portmap.GenericPortmapClient;
 import org.dcache.oncrpc4j.portmap.OncRpcPortmap;
 import org.dcache.oncrpc4j.portmap.OncRpcbindServer;
-import org.dcache.oncrpc4j.portmap.rpcb;
 import org.junit.Test;
 
 public class TestGitHubIssue56 {
 
-	@Test
-	public void DumpTest() throws IOException, TimeoutException {
+    @Test
+    public void DumpTest() throws IOException, TimeoutException {
 
-		OncRpcSvc rpcbindServer = new OncRpcSvcBuilder()
+        OncRpcSvc rpcbindServer = new OncRpcSvcBuilder()
                 .withTCP()
                 .withUDP()
                 .withoutAutoPublish()
                 .withRpcService(new OncRpcProgram(OncRpcPortmap.PORTMAP_PROGRAMM, OncRpcPortmap.PORTMAP_V2), new OncRpcbindServer())
                 .build();
-		rpcbindServer.start();
-		int protoType = IpProtocolType.TCP;
-		OncRpcClient rpcClient = new OncRpcClient(rpcbindServer.getInetSocketAddress(protoType),protoType );
-		RpcTransport transport = rpcClient.connect();
-		GenericPortmapClient portmapClient = new GenericPortmapClient(transport);
-		for (rpcb r : portmapClient.dump()){
-			assertEquals("superuser",r.getOwner());
-		}
-		
-	}
-	@Test
-	public void UnsetTest() throws IOException, TimeoutException {
+        rpcbindServer.start();
+        int protoType = IpProtocolType.TCP;
+        OncRpcClient rpcClient = new OncRpcClient(rpcbindServer.getInetSocketAddress(protoType), protoType);
+        RpcTransport transport = rpcClient.connect();
+        GenericPortmapClient portmapClient = new GenericPortmapClient(transport);
+        assertFalse("Newly started OncRpcbindServer must publish himself", portmapClient.dump().isEmpty());
+    }
 
-		OncRpcSvc rpcbindServer = new OncRpcSvcBuilder()
+    @Test
+    public void SetUnsetTest() throws IOException, TimeoutException {
+
+        OncRpcSvc rpcbindServer = new OncRpcSvcBuilder()
                 .withTCP()
                 .withUDP()
                 .withoutAutoPublish()
                 .withRpcService(new OncRpcProgram(OncRpcPortmap.PORTMAP_PROGRAMM, OncRpcPortmap.PORTMAP_V2), new OncRpcbindServer())
                 .build();
-		rpcbindServer.start();
-		int protoType = IpProtocolType.TCP;
-		OncRpcClient rpcClient = new OncRpcClient(rpcbindServer.getInetSocketAddress(protoType),protoType );
-		RpcTransport transport = rpcClient.connect();
-		GenericPortmapClient portmapClient = new GenericPortmapClient(transport);
-		boolean isUnset=portmapClient.unsetPort(OncRpcPortmap.PORTMAP_PROGRAMM, OncRpcPortmap.PORTMAP_V2, "superuser");
-		assertTrue(isUnset);
-		//NPE when dumping an empry portmapper registrar
-		assertEquals(0,portmapClient.dump().size());
-		boolean isSet=portmapClient.setPort(OncRpcPortmap.PORTMAP_PROGRAMM, OncRpcPortmap.PORTMAP_V2,"tcp","127.0.0.1.0.234", "superuser");
-		assertTrue(isSet);
-		assertEquals(1,portmapClient.dump().size());
-	}
-	
+        rpcbindServer.start();
+        int protoType = IpProtocolType.TCP;
+        OncRpcClient rpcClient = new OncRpcClient(rpcbindServer.getInetSocketAddress(protoType), protoType);
+        RpcTransport transport = rpcClient.connect();
+        GenericPortmapClient portmapClient = new GenericPortmapClient(transport);
+
+        int n = portmapClient.dump().size();
+        assertTrue(portmapClient.setPort(9000001, 1, "tcp", "127.0.0.1.0.234", "superuser"));
+        assertEquals(n + 1, portmapClient.dump().size());
+        assertTrue(portmapClient.unsetPort(9000001, 1, "superuser"));
+        assertEquals(n, portmapClient.dump().size());
+    }
 }
